09d8ec695a1166ba807565baf71ce972319c35e4
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2021-12-08 16:40:23 +0100

    add nav component

---
+++ src/Components/Nav.js
@@ -0,0 +1,8 @@
+import { Component } from "react/cjs/react.production.min";
+
+class Nav extends Component {
+  render() {
+    return <nav className={this.props.css}></nav>;
+  }
+}
+export default Nav;
