89b9fc73b3a50085b451bdff5c78376ef10292f6
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2021-12-08 18:21:01 +0100

    add header component

---
+++ src/Components/Header.js
@@ -0,0 +1,15 @@
+import { Component } from "react/cjs/react.production.min";
+
+class Header extends Component {
+  render() {
+    return (
+      <header>
+        {/* test code for now */}
+        <h2>User</h2>
+        <h2>xp</h2>
+        <h2>notifications</h2>
+      </header>
+    );
+  }
+}
+export default Header;
