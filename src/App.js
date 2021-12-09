8ef8b65b330d6bb4c0d8c165c9eb97ad0cb2305a
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2021-12-09 15:21:22 +0100

    add basic header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
+import Header from "./Components/Header";
 import Nav from "./Components/Nav";
 
 function App() {
   return (
     <div className="App container-fluid">
       <Nav Css="left-menu d-flex" />
+      <Header />
     </div>
   );
 }
