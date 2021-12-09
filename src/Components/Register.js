09bc6dded1209bc7d490b2f5d7cfd99c50c4ee0f
MohamedBechirMejri <76930306+MohamedBechirMejri@users.noreply.github.com>
2021-12-09 22:35:06 +0100

    add register component

---
+++ src/Components/Register.js
@@ -0,0 +1,15 @@
+import { Component } from "react";
+import Input from './SubComponent/Input'
+class Register extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Register</h1>
+        <form>
+            <input />
+        </form>
+      </div>
+    );
+  }
+}
+export default Register;
