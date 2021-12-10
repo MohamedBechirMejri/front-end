d8ae42f9ee42b190ca068770957d35a0126b221a
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2021-12-10 21:54:30 +0100

    finish register page styling again

--- src/Components/Register.js
+++ src/Components/Register.js
@@ -2,11 +2,24 @@ import { Component } from "react";
 import "../Styles/register.scss";
 import Input from "./SubComponents/Input";
 import Button from "./SubComponents/Button";
-// import Footer from "./Footer";
+
 class Register extends Component {
+  constructor(props) {
+    super(props);
+    this.showForm = this.showForm.bind(this);
+    this.state = {
+      isFormHidden: true,
+    };
+  }
+  showForm() {
+    this.setState({
+      isFormHidden: false,
+    });
+  }
   render() {
     return (
       <div className="register">
+        <h1>FightMe.tn</h1>
         <h2>Create a new account</h2>
         <div className="oauth-buttons">
           <Button
@@ -41,8 +54,8 @@ class Register extends Component {
           <hr />
         </div>
         <h2>Subscribe with email</h2>
-        <Button ButtonName="continue with  Email" OnClick />
-        <form>
+        <Button ButtonName="continue with  Email" OnClick={this.showForm} />
+        <form className={this.state.isFormHidden && "hidden"}>
           <Input Name="username" Placeholder="Username" Type="text" OnChange />
           <Input Name="email" Placeholder="Email" Type="email" OnChange />
           <Input
@@ -57,11 +70,15 @@ class Register extends Component {
             Type="password"
             OnChange
           />
-          <Input Name="terms" Placeholder="You agree to our Terms" Type="checkbox" OnChange />
+          <Input
+            Name="terms"
+            Placeholder="You agree to our Terms"
+            Type="checkbox"
+            OnChange
+          />
 
           <Button ButtonName="Join Now" OnClick />
         </form>
-        {/*   <Footer/> */}
       </div>
     );
   }
