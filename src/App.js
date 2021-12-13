f52b6b00e3a5fe81ce9ce22875a002d080ef8e0e
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2021-12-13 02:14:47 +0100

    add login page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import "./App.css";
 import Footer from "./Components/Footer";
+import Login from "./Components/Login";
 // import Header from "./Components/Header";
 // import Menu from "./Components/Menu";
-import Register from "./Components/Register";
+// import Register from "./Components/Register";
 
 function App() {
   return (
     <div className="App">
       {/*   <Menu Css="left-menu d-flex" /> */}
       {/*  <Header /> */}
-      <Register />
+      {/* <Register /> */}
+      <Login/>
       <Footer />
     </div>
   );
---
+++ src/Components/Login.js
@@ -0,0 +1,13 @@
+
+import { Component } from 'react/cjs/react.production.min';
+
+class Login extends Component {
+    render() {
+        return (
+            <div className='login'>
+                
+            </div>
+        );
+    }
+}
+export default Login;
