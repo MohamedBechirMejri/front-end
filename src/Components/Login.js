bcea9150ebd800d809c17e73c669eb210292321d
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2021-12-13 02:24:21 +0100

    add login components

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,13 +1,50 @@
-
-import { Component } from 'react/cjs/react.production.min';
+import { Component } from "react/cjs/react.production.min";
+import OAuthButtons from "./Register-Login/OAuthButtons";
+import Or from "./Register-Login/Or";
+import Input from "./SubComponents/Input";
+import Button from "./SubComponents/Button";
 
 class Login extends Component {
-    render() {
-        return (
-            <div className='login'>
-                
-            </div>
-        );
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            
+        }
     }
+  render() {
+    return (
+      <div className="login">
+        <h2>Login With</h2>
+        <h3>your FightMe.tn account</h3>
+        <form>
+          <Input
+            Name="email"
+            Placeholder="Email"
+            Type="email"
+            OnChange={this.changeEmailInput}
+          />
+          <Input
+            Name="password"
+            Placeholder="Password"
+            Type="password"
+            OnChange={this.changePasswordInput}
+          />
+          <Input
+            Name="nosession"
+            Placeholder="Stay logged in"
+            Type="checkbox"
+          />
+          <Button ButtonName="Login" OnClick={this.submitForm} />
+          <p className="message">{this.state.serverMessage}</p>
+          <p>
+            <a href="/register">Not a member yet?</a>
+          </p>
+        </form>
+        <Or />
+        <OAuthButtons />
+      </div>
+    );
+  }
 }
 export default Login;
