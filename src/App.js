de5e067091b85d56d06d79145a507126f40868ac
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2021-12-14 14:39:39 +0100

    add home page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Header from "./Components/Header";
 import Menu from "./Components/Menu";
 import Register from "./Components/Register";
 import { Routes, Route } from "react-router-dom";
+import Home from './Components/Home';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Menu Css="left-menu d-flex" />
       <Header />
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="login" element={<Login />} />
         <Route path="register" element={<Register />} />
       </Routes>
---
+++ src/Components/Home.js
@@ -0,0 +1,19 @@
+import Button from "./SubComponents/Button";
+import { Link } from "react-router-dom";
+import { Component } from "react/cjs/react.production.min";
+
+class Home extends Component {
+  render() {
+    return (
+      <div className="home">
+        <i className="bi bi-cone-striped" />
+        <h1>This Page is under Construction</h1>
+        <div className="login-register-buttons">
+          <Link to="register">{<Button ButtonName="Register" />}</Link>
+          <Link to="login">{<Button ButtonName="Login" />}</Link>
+        </div>
+      </div>
+    );
+  }
+}
+export default Home;
