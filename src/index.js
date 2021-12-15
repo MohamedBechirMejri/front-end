a0659012286facfd0241bd51558ee021715a45f9
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2021-12-15 02:30:20 +0100

    test usecontext

--- src/index.js
+++ src/index.js
@@ -2,14 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-
+import userData from "./Auth/useContext";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
+    {" "}
     <BrowserRouter>
-      <App />
+      <userData.Provider value={null}>
+        <App />
+      </userData.Provider>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
