0ed02dce72a75b3ddc05d7f0313e1265ad90df33
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2021-12-15 02:14:14 +0100

    test stuff

---
+++ src/Auth/useContext.js
@@ -0,0 +1,20 @@
+import React from "react";
+// eslint-disable-next-line no-unused-vars
+import axios from "axios";
+
+
+//////////////////////////////////
+// REPLACE THIS WITH WORKING CODE
+//////////////////////////////////
+
+
+const grabUser = () => {
+  // eslint-disable-next-line no-unused-vars
+  //const user = axios("do this later");
+  return '';
+  //return user.data;
+};
+
+const userData = React.createContext(grabUser());
+
+export default userData;
