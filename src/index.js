f24397638a5abd30764aa87c92f5ba299f8fbdd6
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2021-12-15 18:49:29 +0100

    remove hooks

--- src/index.js
+++ src/index.js
@@ -2,17 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import userData from "./Auth/useContext";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    {" "}
     <BrowserRouter>
-      <userData.Provider value={null}>
-        <App />
-      </userData.Provider>
+      <App />
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
