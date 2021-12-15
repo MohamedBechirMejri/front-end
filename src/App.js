b4bb2a713eef1e14d81c35fa9f12d5487a5dd9a6
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2021-12-15 02:13:09 +0100

    redirections if authenticated or not

--- src/App.js
+++ src/App.js
@@ -4,22 +4,44 @@ import Login from "./Components/Login";
 import Header from "./Components/Header";
 import Menu from "./Components/Menu";
 import Register from "./Components/Register";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Home from "./Components/Home";
+import userData from "./Auth/useContext";
+import { useContext } from "react";
 
-function App() {
+const App = () => {
+  const user = useContext(userData);
+
   return (
     <div className="App">
-      <Menu Css="left-menu d-flex" />
-      <Header />
+      {user && <Menu Css="left-menu d-flex" />}
+      {user && <Header />}
+
       <Routes>
-        <Route path="/" element={<Home />} />
         <Route path="login" element={<Login />} />
         <Route path="register" element={<Register />} />
+        <Route
+          path="/"
+          element={
+            user ? <Navigate to="/dashboard" replace={true} /> : <Home />
+          }
+        />
+        <Route
+          path="dashboard"
+          element={!user ? <Navigate to="/login" replace={true} /> : <Home />}
+        />
+        <Route
+          path="tournaments"
+          element={!user ? <Navigate to="/login" replace={true} /> : <Home />}
+        />
+        <Route
+          path="teams"
+          element={!user ? <Navigate to="/login" replace={true} /> : <Home />}
+        />
       </Routes>
       <Footer />
     </div>
   );
-}
+};
 
 export default App;
