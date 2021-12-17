93ce3fcf24e0baef87b19f6996b5dd67fb3aeab5
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2021-12-18 00:18:51 +0100

    import games.scss

--- src/Components/Home/Games.js
+++ src/Components/Home/Games.js
@@ -1,12 +1,15 @@
 import Game from "./Game";
 import uniqid from "uniqid";
-import "../../Styles/Games.scss";
+import "./Styles/Games.scss";
 const Games = (props) => {
   return (
     <div className="games-section">
-      {props.Games.map((game) => (
-        <Game key={uniqid()} game={game} />
-      ))}
+      <h3 className="title">Top Games</h3>
+      <div className="games-container">
+        {props.Games.map((game) => (
+          <Game key={uniqid()} game={game} />
+        ))}
+      </div>
     </div>
   );
 };
