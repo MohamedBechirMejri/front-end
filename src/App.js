364011b0479baea1b2cbd4f586fe110fcca2e142
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2021-12-18 00:02:08 +0100

    test main page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import "./App.css";
 import Footer from "./Components/Footer";
-import Login from "./Components/Login";
+// import Login from "./Components/Login";
 import Header from "./Components/Header";
 import Menu from "./Components/Menu";
-import Register from "./Components/Register";
-import { Routes, Route, Navigate } from "react-router-dom";
+// import Register from "./Components/Register";
+// import { Routes, Route, Navigate } from "react-router-dom";
 import Home from "./Components/Home";
 import { Component } from "react/cjs/react.production.min";
 import axios from "axios";
-import { Toaster } from "react-hot-toast";
+// import { Toaster } from "react-hot-toast";
 
 class App extends Component {
   constructor(props) {
@@ -53,71 +53,79 @@ class App extends Component {
       });
   }
   render() {
-    const { user } = this.state;
+    // const { user } = this.state;
     return (
       <div className="App">
-        <Toaster position="top-right" reverseOrder={true} />
-        {user && <Menu Css="left-menu d-flex" />}
-        {user && <Header />}
-
-        <Routes>
-          <Route
-            path="login"
-            element={
-              user ? (
-                <Navigate to="/dashboard" replace={true} />
-              ) : (
-                <Login SetToken={this.setToken} GrabUser={this.grabUser} />
-              )
-            }
-          />
-          <Route
-            path="register"
-            element={
-              user ? <Navigate to="/dashboard" replace={true} /> : <Register />
-            }
-          />
-          <Route
-            path="/"
-            element={
-              user ? <Navigate to="/dashboard" replace={true} /> : <Home />
-            }
-          />
-          <Route
-            path="dashboard"
-            element={
-              !user ? (
-                <Navigate to="/login" replace={true} />
-              ) : (
-                <Home User={user} />
-              )
-            }
-          />
-          <Route
-            path="tournaments"
-            element={
-              !user ? (
-                <Navigate to="/login" replace={true} />
-              ) : (
-                <Home User={user} />
-              )
-            }
-          />
-          <Route
-            path="teams"
-            element={
-              !user ? (
-                <Navigate to="/login" replace={true} />
-              ) : (
-                <Home User={user} />
-              )
-            }
-          />
-        </Routes>
-
+        <Menu Css="left-menu d-flex" />
+        <Header />
+        <Home />
         <Footer />
       </div>
     );
+    // return (
+    //   <div className="App">
+    //     <Toaster position="top-right" reverseOrder={true} />
+    //     {user && <Menu Css="left-menu d-flex" />}
+    //     {user && <Header />}
+
+    //     <Routes>
+    //       <Route
+    //         path="login"
+    //         element={
+    //           user ? (
+    //             <Navigate to="/dashboard" replace={true} />
+    //           ) : (
+    //             <Login SetToken={this.setToken} GrabUser={this.grabUser} />
+    //           )
+    //         }
+    //       />
+    //       <Route
+    //         path="register"
+    //         element={
+    //           user ? <Navigate to="/dashboard" replace={true} /> : <Register />
+    //         }
+    //       />
+    //       <Route
+    //         path="/"
+    //         element={
+    //           user ? <Navigate to="/dashboard" replace={true} /> : <Home />
+    //         }
+    //       />
+    //       <Route
+    //         path="dashboard"
+    //         element={
+    //           !user ? (
+    //             <Navigate to="/login" replace={true} />
+    //           ) : (
+    //             <Home User={user} />
+    //           )
+    //         }
+    //       />
+    //       <Route
+    //         path="tournaments"
+    //         element={
+    //           !user ? (
+    //             <Navigate to="/login" replace={true} />
+    //           ) : (
+    //             <Home User={user} />
+    //           )
+    //         }
+    //       />
+    //       <Route
+    //         path="teams"
+    //         element={
+    //           !user ? (
+    //             <Navigate to="/login" replace={true} />
+    //           ) : (
+    //             <Home User={user} />
+    //           )
+    //         }
+    //       />
+    //     </Routes>
+
+    //     <Footer />
+    //   </div>
+    // );
   }
 }
 
