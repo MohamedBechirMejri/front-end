883e6ce85a018cd576347a868ad0ec06adf9b9ad
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2021-12-18 00:02:22 +0100

    add home components

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,18 +1,48 @@
-import Button from "./SubComponents/Button";
-import { Link } from "react-router-dom";
+// import Button from "./SubComponents/Button";
+// import { Link } from "react-router-dom";
 import { Component } from "react/cjs/react.production.min";
 import "../Styles/home.scss";
+import Games from "./Home/Games";
+import Slider from "./Home/Slider";
+import Tournaments from "./Home/Tournaments";
 
 class Home extends Component {
   render() {
     return (
       <div className="home">
-        <i className="bi bi-cone-striped" />
+        <Slider />
+        <Games
+          Games={[
+            {
+              image:
+                "https://umg-uploads.s3.us-east-2.amazonaws.com/uploads/system/games/266e548a254082fa1c77625223e64440.png",
+              title: "League of Legends",
+            },
+            {
+              image:
+                "https://umg-uploads.s3.us-east-2.amazonaws.com/uploads/system/games/266e548a254082fa1c77625223e64440.png",
+              title: "League of Legends",
+            },
+            {
+              image:
+                "https://umg-uploads.s3.us-east-2.amazonaws.com/uploads/system/games/266e548a254082fa1c77625223e64440.png",
+              title: "League of Legends",
+            },
+            {
+              image:
+                "https://umg-uploads.s3.us-east-2.amazonaws.com/uploads/system/games/266e548a254082fa1c77625223e64440.png",
+              title: "League of Legends",
+            },
+          ]}
+        />
+        <Tournaments />
+
+        {/* <i className="bi bi-cone-striped" />
         <h1>This Page is under Construction</h1>
         <div className="login-register-buttons">
           <Link to="register">{<Button ButtonName="Register" />}</Link>
           <Link to="login">{<Button ButtonName="Login" />}</Link>
-        </div>
+        </div> */}
       </div>
     );
   }
