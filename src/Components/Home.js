df7c39718260c340e91b11f8fbf10fe0d85b420c
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2021-12-18 01:15:33 +0100

    add more games for testing

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -33,6 +33,26 @@ class Home extends Component {
                 "https://umg-uploads.s3.us-east-2.amazonaws.com/uploads/system/games/266e548a254082fa1c77625223e64440.png",
               title: "League of Legends",
             },
+            {
+              image:
+                "https://umg-uploads.s3.us-east-2.amazonaws.com/uploads/system/games/266e548a254082fa1c77625223e64440.png",
+              title: "League of Legends",
+            },
+            {
+              image:
+                "https://umg-uploads.s3.us-east-2.amazonaws.com/uploads/system/games/266e548a254082fa1c77625223e64440.png",
+              title: "League of Legends",
+            },
+            {
+              image:
+                "https://umg-uploads.s3.us-east-2.amazonaws.com/uploads/system/games/266e548a254082fa1c77625223e64440.png",
+              title: "League of Legends",
+            },
+            {
+              image:
+                "https://umg-uploads.s3.us-east-2.amazonaws.com/uploads/system/games/266e548a254082fa1c77625223e64440.png",
+              title: "League of Legends",
+            },
           ]}
         />
         <Tournaments />
