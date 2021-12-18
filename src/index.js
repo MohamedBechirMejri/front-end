c750cc3ba1c7e9fa5439b0797449d993b13bdbbc
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2021-12-18 21:05:31 +0100

    create new app component

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./App";
+import App from "../Old/src/App";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import { BrowserRouter } from "react-router-dom";
 
