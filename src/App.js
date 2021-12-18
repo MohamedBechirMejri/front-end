1485e7fd7d83bdac3ce1f1ae024bbfdefe051ddc
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2021-12-18 23:49:12 +0100

    style header

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import Header from "./Components/Header";
 import UserBubble from "./Components/UserBubble";
 const App = () => {
   return (
     <div className="App">
+      <Header />
       <UserBubble />
     </div>
   );
