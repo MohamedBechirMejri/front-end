f7e6380606611b1fc7c4b1b89d04e1f7958ed188
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2021-12-19 03:02:51 +0100

    change to func component

--- src/Components/SubComponents/Button.js
+++ src/Components/SubComponents/Button.js
@@ -1,13 +1,10 @@
-import { Component } from "react/cjs/react.production.min";
+const Button = props => {
+  return (
+    <button onClick={props.OnClick} className={props.ClassName}>
+      {props.Icon && <i className={"bi " + props.Icon} />}
+      {props.ButtonName}
+    </button>
+  );
+};
 
-class Button extends Component {
-  render() {
-    return (
-      <button onClick={this.props.OnClick} className={this.props.ClassName}>
-        {this.props.Icon && <i className={"bi " + this.props.Icon} />}
-        {this.props.ButtonName}
-      </button>
-    );
-  }
-}
 export default Button;
