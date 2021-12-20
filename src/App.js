c90b42e362caedae6bff87aab2c49afb86c3e034
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2021-12-20 15:58:01 +0100

    remove useless files

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import Header from "./Components/Header";
 import Login from "./Components/Header/Login";
 import Home from "./Components/Home";
-import UserBubble from "./Components/UserBubble";
 const App = () => {
   return (
     <div className="App">
       <Header />
-      <UserBubble />
+
       <Home />
       <Login />
     </div>
