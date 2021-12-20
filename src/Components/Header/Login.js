31a5a2a4d23e3975d7c6b441861967503061d536
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2021-12-20 16:10:43 +0100

    style buttons using tailwind

--- src/Components/Header/Login.js
+++ src/Components/Header/Login.js
@@ -7,21 +7,21 @@ const Login = () => {
       <h1>Login With</h1>
       <div className="social-media">
         <Button
-          ButtonName="Discord"
+          ButtonName=""
           OnClick={() => console.log("hi")}
-          ClassName="discord-button"
+          ClassName="relative flex justify-center w-full px-4 py-2 text-sm font-medium text-white bg-indigo-600 border border-transparent rounded-md discord-button group hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
           Icon="bi-discord"
         />
         <Button
-          ButtonName="Google"
+          ButtonName=""
           OnClick={() => console.log("hi")}
-          ClassName="google-button"
+          ClassName="relative flex justify-center w-full px-4 py-2 text-sm font-medium text-white bg-indigo-600 border border-transparent rounded-md google-button group hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
           Icon="bi-google"
         />
         <Button
-          ButtonName="Facebook"
+          ButtonName=""
           OnClick={() => console.log("hi")}
-          ClassName="facebook-button"
+          ClassName="relative flex justify-center w-full px-4 py-2 text-sm font-medium text-white bg-indigo-600 border border-transparent rounded-md facebook-button group hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
           Icon="bi-facebook"
         />
       </div>
