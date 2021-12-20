8d991ec2a2d371287f3dda23bcceede3d0422ab8
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2021-12-20 18:31:04 +0100

    tailwind login componennt

--- src/Components/Header/Login.js
+++ src/Components/Header/Login.js
@@ -1,31 +1,89 @@
 import "../../Styles/Header/Login.scss";
 import Button from "../SubComponents/Button";
 
-const Login = () => {
+/* This example requires Tailwind CSS v2.0+ */
+import { Fragment, useRef, useState } from "react";
+import { Dialog, Transition } from "@headlessui/react";
+
+export default function Example() {
+  const [open, setOpen] = useState(true);
+
+  const cancelButtonRef = useRef(null);
+
   return (
-    <div className="login">
-      <h1>Login With</h1>
-      <div className="social-media">
-        <Button
-          ButtonName=""
-          OnClick={() => console.log("hi")}
-          ClassName="relative flex justify-center w-full px-4 py-2 text-sm font-medium text-white bg-indigo-600 border border-transparent rounded-md discord-button group hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-          Icon="bi-discord"
-        />
-        <Button
-          ButtonName=""
-          OnClick={() => console.log("hi")}
-          ClassName="relative flex justify-center w-full px-4 py-2 text-sm font-medium text-white bg-indigo-600 border border-transparent rounded-md google-button group hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-          Icon="bi-google"
-        />
-        <Button
-          ButtonName=""
-          OnClick={() => console.log("hi")}
-          ClassName="relative flex justify-center w-full px-4 py-2 text-sm font-medium text-white bg-indigo-600 border border-transparent rounded-md facebook-button group hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-          Icon="bi-facebook"
-        />
-      </div>
-    </div>
+    <Transition.Root show={open} as={Fragment}>
+      <Dialog
+        as="div"
+        className="fixed inset-0 z-10 overflow-y-auto"
+        initialFocus={cancelButtonRef}
+        onClose={setOpen}
+      >
+        <div className="flex items-end justify-center min-h-screen px-4 pt-4 pb-20 text-center sm:block sm:p-0">
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <Dialog.Overlay className="fixed inset-0 transition-opacity bg-gray-500 bg-opacity-75" />
+          </Transition.Child>
+
+          {/* This element is to trick the browser into centering the modal contents. */}
+          <span
+            className="hidden sm:inline-block sm:align-middle sm:h-screen"
+            aria-hidden="true"
+          >
+            &#8203;
+          </span>
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+            enterTo="opacity-100 translate-y-0 sm:scale-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+            leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+          >
+            <div className="inline-block overflow-hidden text-left align-bottom transition-all transform bg-white rounded-lg shadow-xl sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
+              <div className="px-4 pt-5 pb-4 bg-white sm:p-6 sm:pb-4">
+                <div className="sm:flex sm:items-start">
+                  <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+                    <Dialog.Title
+                      as="h3"
+                      className="text-lg font-medium leading-6 text-gray-900"
+                    >
+                      Login With
+                    </Dialog.Title>
+                  </div>
+                </div>
+              </div>
+              <div className="px-4 py-3 bg-gray-50 sm:px-6 sm:flex sm:flex-row-reverse">
+                <Button
+                  ButtonName=""
+                  OnClick={() => console.log("hi")}
+                  ClassName="bg-[#5865f2]"
+                  Icon="bi-discord"
+                />
+                <Button
+                  ButtonName=""
+                  OnClick={() => console.log("hi")}
+                  ClassName="bg-[#ea4335]"
+                  Icon="bi-google"
+                />
+                <Button
+                  ButtonName=""
+                  OnClick={() => console.log("hi")}
+                  ClassName="bg-[#1877f2]"
+                  Icon="bi-facebook"
+                />
+              </div>
+            </div>
+          </Transition.Child>
+        </div>
+      </Dialog>
+    </Transition.Root>
   );
-};
-export default Login;
+}
--- src/Components/SubComponents/Button.js
+++ src/Components/SubComponents/Button.js
@@ -1,6 +1,12 @@
-const Button = props => {
+const Button = (props) => {
   return (
-    <button onClick={props.OnClick} className={props.ClassName}>
+    <button
+      onClick={props.OnClick}
+      className={
+        props.ClassName +
+        " relative flex justify-center w-full px-4 py-2 text-sm font-medium text-white  border border-transparent rounded-md discord-button group  focus:outline-none focus:ring-2 focus:ring-offset-2 "
+      }
+    >
       {props.Icon && <i className={"bi " + props.Icon} />}
       {props.ButtonName}
     </button>
