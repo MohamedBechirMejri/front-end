b800c401e4d9d9b0d38dab2d4165a4bc639969d0
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2021-12-21 13:48:26 +0100

    add carousel

--- src/Components/Dashboard/Carousel.js
+++ src/Components/Dashboard/Carousel.js
@@ -12,6 +12,41 @@ import SwiperCore, { Autoplay, Pagination, Navigation } from "swiper";
 // install Swiper modules
 SwiperCore.use([Autoplay, Pagination, Navigation]);
 
+const events = [
+  {
+    title: "Event 1",
+    image: "https://picsum.photos/1920/1080",
+    date: "2020-01-01",
+    time: "10:00",
+    location: "Location 1",
+    description: "Description 1",
+  },
+  {
+    title: "Event 2",
+    image: "https://picsum.photos/1920/1080",
+    date: "2020-01-01",
+    time: "10:00",
+    location: "Location 2",
+    description: "Description 2",
+  },
+  {
+    title: "Event 3",
+    image: "https://picsum.photos/1920/1080",
+    date: "2020-01-01",
+    time: "10:00",
+    location: "Location 3",
+    description: "Description 3",
+  },
+  {
+    title: "Event 4",
+    image: "https://picsum.photos/1920/1080",
+    date: "2020-01-01",
+    time: "10:00",
+    location: "Location 4",
+    description: "Description 4",
+  },
+];
+
 const Carousel = () => {
   return (
     <>
@@ -28,15 +63,11 @@ const Carousel = () => {
         navigation={true}
         className="mySwiper"
       >
-        <SwiperSlide>Slide 1</SwiperSlide>
-        <SwiperSlide>Slide 2</SwiperSlide>
-        <SwiperSlide>Slide 3</SwiperSlide>
-        <SwiperSlide>Slide 4</SwiperSlide>
-        <SwiperSlide>Slide 5</SwiperSlide>
-        <SwiperSlide>Slide 6</SwiperSlide>
-        <SwiperSlide>Slide 7</SwiperSlide>
-        <SwiperSlide>Slide 8</SwiperSlide>
-        <SwiperSlide>Slide 9</SwiperSlide>
+        {events.map((event) => (
+          <SwiperSlide key={event.title}>
+            <img src={event.image} />
+          </SwiperSlide>
+        ))}
       </Swiper>
     </>
   );
