17e363bbcc243ed072f253e259764990bbc308b1
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2021-12-21 15:30:12 +0100

    style games

--- src/Components/Dashboard/Games.js
+++ src/Components/Dashboard/Games.js
@@ -1,11 +1,10 @@
 import uniqid from "uniqid";
-import Game from "./Game";
-import './Styles/Games.scss';
+import Game from "./Game"; 
 const Games = (props) => {
   return (
-    <div className="games">
-      <h3 className="title">Top Games</h3>
-      <div className="games-container">
+    <div className="flex-col gap-4 m-4 select-none">
+      <h3 className="text-lg font-extrabold">Top Games</h3>
+      <div className="flex max-w-full gap-4 py-4 my-4 overflow-x-scroll justify-evenly h-max">
         {props.Games.map((game) => (
           <Game key={uniqid()} game={game} />
         ))}
